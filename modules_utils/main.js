a2468327a67b0fe2bf8865e5824affc6104bec13
heyheynic <nicsayhey@gmail.com>
2024-09-20 10:12:49 +0200

    module utils

---
+++ modules_utils/main.js
@@ -0,0 +1,27 @@
+import {
+  determineWinner,
+  getComputerChoice,
+  optionsArray,
+} from "./gameLogic.js";
+import { displayResult, createButtons } from "./ui.js";
+
+const buttonsContainer = document.querySelector(".btns");
+
+// join("") fjerner punktum mellem knapperne, og space giver dem gap 
+buttonsContainer.innerHTML = createButtons(optionsArray).join(" ");
+
+
+// Hvis det button jeg klikker på ikke har option/data choice, så stopper vi. "!e" betyder hvis noget ikke er sant/rigtigt. 
+buttonsContainer.addEventListener("click", (e) => {
+  if (!e.target.dataset.choice) {
+    return;
+  }
+
+  playGame(e.target.dataset.choice);
+});
+
+function playGame(userChoice) {
+  const computerChoice = getComputerChoice();
+  const result = determineWinner(userChoice, computerChoice);
+  displayResult(userChoice, computerChoice, result, ".result");
+}
